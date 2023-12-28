f2893702340977a762bd9078f487d876dd04ea98
lamuska <lamuska@DESKTOP-086P3MT>
2023-12-28 11:16:30 +0500

    Fix Calc.php, composer.json, README.md

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 [![Actions Status](https://github.com/Lamuska/php-project-45/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Lamuska/php-project-45/actions)
 <a href="https://codeclimate.com/github/Lamuska/php-project-45/maintainability"><img src="https://api.codeclimate.com/v1/badges/bcf3e93b3101dfc7378e/maintainability" /></a>
 
-Install:
+### Install:
 $ git clone git@github.com:Lamuska/php-project-45.git
 
-Games:
+### Games:
 $ make install
 
 $ make brain-even
--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -9,17 +9,6 @@ const MAX_INTEGER = 30;
 const OPERATORS = ['+', '-', '*'];
 const DESCRIPTION = "What is the result of the expression?";
 
-function getInteger(): int
-{
-    return random_int(MIN_INTEGER, MAX_INTEGER);
-}
-
-function getOperator(): string
-{
-    $key = array_rand(OPERATORS);
-    return OPERATORS[$key];
-}
-
 function getExpressionValue(int $int1, int $int2, string $operator): array
 {
     $question = "";
@@ -45,9 +34,9 @@ function getExpressionValue(int $int1, int $int2, string $operator): array
 function run(): void
 {
     $gameData = function (): array {
-        $int1 = getInteger();
-        $int2 = getInteger();
-        $operator = getOperator();
+        $int1 = random_int(MIN_INTEGER, MAX_INTEGER);
+        $int2 = random_int(MIN_INTEGER, MAX_INTEGER);
+        $operator = OPERATORS[array_rand(OPERATORS)];
         return getExpressionValue($int1, $int2, $operator);
     };
     play($gameData, DESCRIPTION);
